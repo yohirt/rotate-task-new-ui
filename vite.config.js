bd23c1eee148eccf8084e6d7d42cf3e5713bfc10
Rafał Taraszka <mail.rtaraszka@gmail.com>
2026-06-18 14:32:21 +0200

    GitHub automatyczny deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: isGithubActions && repoName ? `/${repoName}/` : '/',
 })
